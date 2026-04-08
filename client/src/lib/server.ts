ad5f57a5c74b558669bd78fb4128bcdda4adf990
tris academy <trisacademybackup@gmail.com>
2026-04-08 11:43:43 +0300

    server

--- client/src/lib/server.ts
+++ client/src/lib/server.ts
@@ -1,3 +1,2 @@
 // export const SERVER_URL = "http://localhost:8000/api";
-export const SERVER_URL =
-  "https://tris-motorcycles-acadamy-backup.onrender.com/api";
+export const SERVER_URL = "https://tris-school-academy-backup.onrender.com/api";
